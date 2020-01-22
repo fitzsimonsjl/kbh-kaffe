86038bb41c387fbab122f6254b2138018f6a50f0
Jack Fitzsimons <fitzsimonsjal@gmail.com>
2020-01-22 10:57:06 +0100

    added some controllers

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+
+root to: "pages#home"
+
+  get 'cafes', to: 'cafes#index'
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
