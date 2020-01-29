3dc5a2491609dc0ff9ab0c7ccb2586202f39173b
Jack Fitzsimons <fitzsimonsjal@gmail.com>
2020-01-29 11:30:16 +0100

    added devise and added some actions to cafes controller

--- app/controllers/cafes_controller.rb
+++ app/controllers/cafes_controller.rb
@@ -1,12 +1,29 @@
 class CafesController < ApplicationController
 
+  # GET /cafes
   def index
   @cafes = Cafe.all
   end
 
+  # GET /cafes/1
   def show
   @cafe = cafe.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
 end
 
+private
+
+def cafe_params
+  params.require(:cafe).permit(:name, :address, :affordability. :rating, :logo_url)
+
+  end
 
