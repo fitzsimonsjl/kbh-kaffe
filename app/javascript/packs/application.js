561e10b08c763f61accb4b2e10bcdcb9e72e1e89
Jack Fitzsimons <fitzsimonsjal@gmail.com>
2020-02-13 15:50:13 +0100

    added plain html solution for now, will do JS later for take me there button

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,10 @@ import { initMapbox } from '../plugins/init_mapbox';
 
 initMapbox();
 
+import {openOSM} from "../packs/take-me-there-btn";
+
+openOSM();
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
---
+++ app/javascript/packs/take-me-there-btn.js
@@ -0,0 +1,8 @@
+// const openOSM = () => {
+//   const button = document.querySelectorAll(".take-me-there-btn");
+//     button.addEventListener('click', function (event)) {
+//      console.log("Clicked");
+//     }
+// }
+
+// export {openOSM};
