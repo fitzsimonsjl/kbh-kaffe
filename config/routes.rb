9bc0ca79e9b2992a64cf0a1ce038963435c5b220
Jack Fitzsimons <fitzsimonsjal@gmail.com>
2020-03-10 13:12:02 +0100

    rough landing page layout done. Just need wiring

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users
-root to: "cafes#index"
+  
+root to: "pages#landing"
 
 get "cafes" , to: 'cafes#index'
 get 'cafes/:id', to: 'cafes#show', as: :cafe
