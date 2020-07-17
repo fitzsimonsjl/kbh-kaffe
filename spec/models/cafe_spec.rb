5034b39de28229fc3953f0d45886b3a195cb40f4
Jack Fitzsimons <fitzsimonsjal@gmail.com>
2020-07-17 14:38:06 +0200

    added and started RSpec tests for cafe model

--- spec/models/cafe_spec.rb
+++ spec/models/cafe_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Cafe, type: :model do
+    it "contains valid attributes" do
+        expect(Cafe.new).to be_valid
+    end
+
+    it "is not valid without a cafe name" do
+    end 
+
+    it "is not valid without an address" do
+    end 
+
+    it "is not valid without an affordability rating" do 
+    end 
+
+    it "is not valid without a review rating" do 
+    end 
+
+    it "is not valid without an image" do 
+    end 
+
+    it "is not valid without a latitude coordinate" do
+    end 
+
+    it "is not valid without a longitude coordinate" do 
+    end 
     
 end
